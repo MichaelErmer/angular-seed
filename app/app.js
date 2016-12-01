8e5c15a46c402d08d63dddb0b8ac4cf490cc5bb9
Michael Ermer <michael.ermer@ermer.de>
2016-12-01 12:42:01 +0100

    feathers

--- app/app.js
+++ app/app.js
@@ -3,11 +3,21 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngFeathers',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+constant('feathersSettings', {
+  endpointUrl: 'http://yourapi.url',
+  useSocket: true
+}).
+config(['$locationProvider', '$routeProvider', '$feathersProvider', 'feathersSettings', function($locationProvider, $routeProvider,$feathersProvider, feathersSettings, ) {
+  $feathersProvider.setEndpoint(feathersSettings.endpointUrl)
+    
+  // true is default; set to false if you like to use REST 
+  $feathersProvider.useSocket(feathersSettings.useSocket);
+
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
