4245365f50aa8eee3400fc87dced169733df57c1
Michael Ermer <michael.ermer@ermer.de>
2016-12-01 13:21:46 +0100

    feathers

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,34 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope, $log, $feathers) {
+	$scope.message = {
+		text: ""
+	};
+	$scope.messages = [];
+	$scope.totalMessagesCount = 0;
 
-}]);
+	$feathers.service('messages').find({query: {}}).then(function(result) {
+		$scope.messages=result.data;
+		$scope.totalMessagesCount = result.total;
+	})
+	.catch(function (error) {
+		$log.error(error);
+	});
+
+	$feathers.service('messages').on('created', function(message) {
+		$scope.messages.push(message);
+		$scope.totalMessages++;
+	});
+
+	$scope.addMessage = function() {
+		$log.log("creating...", $scope.message);
+		$feathers.service('messages').create($scope.message).then(function(message) {
+			$log.log("messages was created", message);
+			$scope.message.text = "";
+		})
+		.catch(function(error) {
+			$log.error(error);
+		});
+	}
+});
